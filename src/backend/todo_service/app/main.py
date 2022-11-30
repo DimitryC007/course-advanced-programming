99eb5bfdbcad9d07f5347ef17f7e30c7bd2c8d72
DmitryChernyak <dimitryc007@gmail.com>
2022-11-30 21:33:08 +0200

    - server

--- src/backend/todo_service/app/main.py
+++ src/backend/todo_service/app/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
 from routers import master
-import uvicorn
 
 app = FastAPI()
 
 app.include_router(master.router)
-
-@app.get("/")
-def read_root():
-    return {"Hello": "todoService"}
-
-if __name__ == '__main__':
-    uvicorn.run(app,host='0.0.0.0',port=8000)
-
